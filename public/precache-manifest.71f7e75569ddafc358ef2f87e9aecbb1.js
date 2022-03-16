81b9019d6f8b13856b0281cb2c8625a3e877e094
leoor22 <leonelrivas.rl@gmail.com>
2022-03-16 12:02:49 -0300

    v1 calendar app

---
+++ public/precache-manifest.71f7e75569ddafc358ef2f87e9aecbb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5616018e5de2a4f00a5007c1bd3d48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "2fdcc23e48179b31b121",
+    "url": "/static/css/main.044e5506.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdcc23e48179b31b121",
+    "url": "/static/js/main.e8b34d88.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
